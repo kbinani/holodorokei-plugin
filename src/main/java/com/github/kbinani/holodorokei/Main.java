c98fb8f1f2f498a0f6c3aea42ffc30452dfb93e9
kbinani <kbinani.bt@gmail.com>
2023-05-03 00:06:05 +0900

    Init

---
+++ src/main/java/com/github/kbinani/holodorokei/Main.java
@@ -0,0 +1,7 @@
+package com.github.kbinani.holodorokei;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+}
