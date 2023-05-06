bd94028d2ae9b2dc499d84b48314a9dd59a65456
kbinani <kbinani.bt@gmail.com>
2023-05-07 03:57:28 +0900

    SkullOwner が将来スキンを切り替えても player_head のスキンが変わらないように対策

--- src/main/java/com/github/kbinani/holodorokei/Area.java
+++ src/main/java/com/github/kbinani/holodorokei/Area.java
@@ -8,7 +8,6 @@ import org.bukkit.block.Chest;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.BoundingBox;
@@ -68,17 +67,29 @@ public abstract class Area {
     this.scheduler = scheduler;
   }
 
-  private static @Nonnull ItemStack CreateHoloXerHead() {
+  private static @Nonnull ItemStack CreateHoloXerHead(World world, Point3i point) {
+    // point の位置にあるチェストを使って player_head アイテムを作る
     if (sPlayerHead == null) {
-      var item = new ItemStack(Material.PLAYER_HEAD);
-      if (item.getItemMeta() instanceof SkullMeta skull) {
-        var server = Bukkit.getServer();
-        var staff1 = server.getOfflinePlayer("kbinani3");
-        skull.setOwningPlayer(staff1);
-        skull.displayName(Component.text("holoXerの頭"));
-        item.setItemMeta(skull);
+      var server = Bukkit.getServer();
+      var sender = server.getConsoleSender();
+      //NOTE: 本当はコマンドではなく paper-api で同じことをやりたい
+      var command = String.format("item replace block %d %d %d container.0 with minecraft:player_head{SkullOwner:{Name:\"kbinani3\",Id:[I;-710913929,-965585696,-1904613534,-1023717780],Properties:{textures:[{Value:\"e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjA2ODFhOTAyMzM4YTBkNjU5ZDY4NTE2ZDI4NGVlMDMwZTM3ZWVjM2RkN2NmN2Y4NTcwZjFlNDI5YTkwOGMzMCJ9fX0=\"}]}}}", point.x, point.y, point.z);
+      server.dispatchCommand(sender, command);
+      var block = world.getBlockAt(point.x, point.y, point.z);
+      if (block.getState() instanceof Chest chest) {
+        var inventory = chest.getInventory();
+        var item = inventory.getItem(0);
+        if (item != null) {
+          var clone = item.clone();
+          var meta = clone.getItemMeta();
+          if (meta != null) {
+            meta.displayName(Component.text("holoXerの頭"));
+            clone.setItemMeta(meta);
+          }
+          sPlayerHead = clone;
+        }
+        inventory.clear();
       }
-      sPlayerHead = item;
     }
     return sPlayerHead.clone();
   }
@@ -109,7 +120,7 @@ public abstract class Area {
         }
         inventory.setItem(13, item);
       } else {
-        var holoXerHead = CreateHoloXerHead();
+        var holoXerHead = CreateHoloXerHead(world, p.position);
         var meta = holoXerHead.getItemMeta();
         if (meta != null) {
           var container = meta.getPersistentDataContainer();
